58445e41c88dbd668ee1fa46cc53ea9ee8c4a9d5
Malik Waseem <malikwaseem123456564@gmail.com>
2026-03-15 17:23:22 +0500

    add

--- Web-Dev-Practice/Call-app/script.js
+++ Web-Dev-Practice/Call-app/script.js
@@ -1,3 +1,4 @@
+// All selected variables
 let addbtn = document.querySelector(".add-btn");
 let noteform = document.querySelector(".Note-form");
 let img = document.querySelector(".image-url");
@@ -8,17 +9,81 @@ let purpose = document.querySelector(".purpose");
 let Bookings = document.querySelector(".Bookings");
 let call = document.querySelector(".call");
 let massage = document.querySelector(".massage");
+let createbtn=document.querySelector(".submit-btn");
+let checkbox=document.querySelectorAll('input[name="Catagory"]');
+let form=document.querySelector("form");
 
-function card() {
-  let card1 = document.createElement("div");
-  card1.classList.add("card-3");
+// This function for Data save in localstorage.
+function savelocalstorage(obj){
+  if(localStorage.getItem("tasks")===null){
+    let oldtasks=[];
+    oldtasks.push(obj);
+    localStorage.setItem("tasks", JSON.stringify(oldtasks));
+  }else{
+    let oldtasks=JSON.parse(localStorage.getItem("tasks"));
+    oldtasks.push(obj);
+    localStorage.setItem("tasks",JSON.stringify(oldtasks));
+  }
+};
 
+//This evevt for Form validation and apply saved data of localstorage and showcards on webpage.
+form.addEventListener("submit",function(e){
+   e.preventDefault();
+   let imageurl=img.value.trim();
+   let hometown=home.value.trim();
+   let namesinput=names.value.trim();
+   let purposes=purpose.value.trim();
+   let selected="";
+   checkbox.forEach(function(val){
+    if(val.checked){
+      selected=val.value;
+    }
+   });
+
+   if(img.value==""){
+    alert("Plz Enter your image URL");
+    return;
+   }
+   if(home.value==""){
+     alert("Plz Enter your hometown");
+     return;
+   }
+   if(names.value==""){
+    alert("Plz Enter your FullName");
+    return;
+   }
+   if(purpose.value==""){
+    alert("Plz enter your purose");
+   }
+   if(!selected){
+    alert("Plz select a catagory");
+    return;
+   }
+   savelocalstorage({
+   imageurl,
+   hometown,
+   namesinput,
+   purposes,
+   selected,
+   });
+   form.reset();
+  createcards( (JSON.parse(localStorage.getItem("tasks"))));
+  noteform.style.display = "none";
+});
+
+
+// This function creating card .
+function  createcards(obj){
+  obj.forEach(element => {
+     let card1 = document.createElement("div");
+  card1.classList.add("card");
+  
   let image = document.createElement("img");
   image.classList.add("img");
-  image.src = img.value;
+  image.src = element.imageurl;
 
   let nm = document.createElement("h2");
-  nm.textContent = names.value;
+  nm.textContent =element.namesinput;
 
   let hometown = document.createElement("h4");
   hometown.classList.add("home-town");
@@ -26,27 +91,23 @@ function card() {
 
   let cityname = document.createElement("h4");
   cityname.classList.add("city");
-  cityname.textContent = home.value;
-
+  cityname.textContent = element.hometown;
 
   let booking = document.createElement("h4");
   booking.textContent = "Booking";
 
   let bookings = document.createElement("h4");
-  bookings.textContent = "3times";
+  bookings.textContent = element.selected;
   bookings.classList.add("Bookings");
-
-
 
   let callbtn = document.createElement("button");
   callbtn.textContent = " Call";
   callbtn.classList.add("call");
 
- 
-
   let massage=document.createElement("h3");
   massage.classList.add("massage");
-  massage.textContent="Massage";
+  massage.textContent=element.purposes;
+
 
   cards.appendChild(card1);
   card1.appendChild(image);
@@ -57,15 +118,12 @@ function card() {
   card1.appendChild(bookings);
   card1.appendChild(callbtn);
   card1.appendChild(massage);
+  });
 }
 
 
-document.querySelector(".submit-btn").addEventListener("click", function (e) {
-  e.preventDefault();
-  card();
-  noteform.style.display = "none";
-});
 
+// This eventlistener used for show form .
 addbtn.addEventListener("click", function (dets) {
   noteform.style.display = "initial";
 });
